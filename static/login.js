36de6c87ad7909bf6da6a5dd45d671177228120e
Pablo Vargas <pvr.mza@gmail.com>
2022-04-26 23:22:49 -0300

    Agregadas paginas login/index

---
+++ static/login.js
@@ -0,0 +1,42 @@
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = './index.html'
+}
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://127.0.0.1:8000/api-token-auth/");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({
+    "username": username,
+    "password": password
+  }));
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      if (objects['token'] ) {
+        localStorage.setItem("jwt", objects['token']);
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = './index.html';
+          }
+        });
+      } else {
+        Swal.fire({
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    }
+  };
+  return false;
+}
